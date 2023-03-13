a2b457c17b2ba5ae6cb7931b73abfbb9aba90e5a
Кирилл Соловьёв <Solovevkirill26072002@ya.ru>
2023-03-13 14:10:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication54.cpp
@@ -0,0 +1,86 @@
+﻿#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct Dlist
+{
+
+	int info;
+	Dlist* next, * prev;
+
+};
+
+
+int main()
+{
+
+	Dlist* head, * p, * r;
+	int x, k, y;
+	int max, i, a, b;
+	float sr;
+	cin >> x;
+	head = new Dlist;
+	head->info = x;
+	p = head;
+
+
+	y = 0;
+
+	cin >> x;
+	while (x)
+	{
+
+		k = 0;
+
+		a = abs(x);
+		while (a > 0)
+		{
+			k++;
+			a /= 10;
+		}
+
+		if (abs(x) / pow(10, k - 1) == 8) y++;
+
+		r = new Dlist;
+		r->info = x;
+		r->prev = p;
+		p->next = r;
+		p = r;
+		cin >> x;
+
+
+	}
+
+	head->prev = p;
+	p->next = head;
+
+	p = head;
+
+
+	sr = 0;
+
+	if (y != 0)
+	{
+
+
+		do
+		{
+
+			if ((p->info < 0) && (p->prev->info % 4 == 0))
+			{
+
+				sr += abs(p->info);
+
+			}
+
+			p = p->next;
+
+		} while (p != head);
+
+	}
+
+	cout << sr;
+
+}
